a40f61b7cc769427a8859fe6cdbb5a0e589c219f
Zihadul Islam <zihadislam2429@gmail.com>
2024-01-08 14:44:28 +0600

    State in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import {useState} from "react";
 import "./App.css";
+import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <h1>React App</h1>
-    </>
+    <div className="border border-purple-500 p-10 m-10">
+      <h1 className="text-xl">Count: {count}</h1>
+      <CounterWithFuncComponent count={count} setCount={setCount} />
+    </div>
   );
 }
 
---
+++ src/components/CounterWithFuncComponent.tsx
@@ -0,0 +1,18 @@
+type TProps = {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const CounterWithFuncComponent = ({count, setCount}: TProps) => {
+  return (
+    <div className="border border-red-500 p-10 m-10">
+      <button
+        className="text-3xl text-white bg-slate-400 rounded-lg font-semibold w-20 h-14"
+        onClick={() => setCount((prev) => prev + 1)}>
+        {count}
+      </button>
+    </div>
+  );
+};
+
+export default CounterWithFuncComponent;

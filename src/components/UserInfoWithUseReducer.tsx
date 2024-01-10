1306987d9193690d51259f11a846fefdaf8ad9a4
Zihadul Islam <zihadislam2429@gmail.com>
2024-01-10 21:58:43 +0600

    dynamic payload and proper usage of TypeScript

--- src/components/UserInfoWithUseReducer.tsx
+++ src/components/UserInfoWithUseReducer.tsx
@@ -1,25 +1,42 @@
-import {useReducer} from "react";
+import {ChangeEvent, useReducer} from "react";
+
+type TAction = {
+  type: string;
+  payload: string;
+};
 
 const initialState = {
   name: "",
   age: "",
-  hobbies: [],
+  hobbies: [] as string[],
 };
 
-const reducer = (currentState, action) => {
+const reducer = (currentState: typeof initialState, action: TAction) => {
   switch (action.type) {
     case "addName":
       return {...currentState, name: action.payload};
+    case "addAge":
+      return {...currentState, age: action.payload};
+    case "addHobby":
+      return {
+        ...currentState,
+        hobbies: [...currentState.hobbies, action.payload],
+      };
     default:
-      break;
+      return currentState;
   }
 };
 
 const UserInfoWithUseReducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state);
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(state);
+  };
+
+  console.log();
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         onChange={(e) => dispatch({type: "addName", payload: e.target.value})}
         className="border border-purple-300 rounded p-1 m-10"
@@ -29,6 +46,7 @@ const UserInfoWithUseReducer = () => {
         placeholder="name"
       />
       <input
+        onChange={(e) => dispatch({type: "addAge", payload: e.target.value})}
         className="border border-purple-300 rounded p-1 m-10"
         type="number"
         name="age"
@@ -36,6 +54,7 @@ const UserInfoWithUseReducer = () => {
         placeholder="age"
       />
       <input
+        onChange={(e) => dispatch({type: "addHobby", payload: e.target.value})}
         className="border border-purple-300 rounded p-1 m-10"
         type="text"
         name="hobbies"

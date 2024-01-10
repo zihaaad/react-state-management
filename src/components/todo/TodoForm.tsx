aa2d8275d951726cbe87d93d9e5cb9afd4c80319
Zihadul Islam <zihadislam2429@gmail.com>
2024-01-10 22:51:29 +0600

    handle Todo

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -1,12 +1,42 @@
-import {useContext} from "react";
+import {FormEvent, useContext, useState} from "react";
 import {TodoContext} from "../../context/TodoProvider";
 
 const TodoForm = () => {
   const {state, dispatch} = useContext(TodoContext);
+  const [task, setTask] = useState("");
+
+  console.log(state);
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random().toString(36).substring(2, 7),
+      title: task,
+      isCompleted: false,
+    };
+
+    dispatch({type: "addTodo", payload: todo});
+  };
 
   return (
-    <div>
-      <h1>This is a todo form component</h1>
+    <div className="text-center">
+      <h1 className="bg-gray-600 font-semibold text-white py-2 ">
+        Todo Submission
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="border rounded px-2 py-3 m-5 w-1/2 border-gray-600 opacity-60"
+          onBlur={(e) => setTask(e.target.value)}
+          type="text"
+          name="todo"
+          placeholder="✍ write TODO here . . . "
+        />
+        <button
+          className="px-2 py-1 text-white bg-gray-500 rounded"
+          type="submit">
+          Add Todo
+        </button>
+      </form>
     </div>
   );
 };
